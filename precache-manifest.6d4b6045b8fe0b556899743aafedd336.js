28c5dcb2686b770aa92799e9263baf3548257190
John Heckbert <johnheckbert@yahoo.ca>
2020-01-01 15:18:22 -0500

    Updates

---
+++ precache-manifest.6d4b6045b8fe0b556899743aafedd336.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b8edb33f63f1e56eaf3719f56dd3a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "29e25cdc603dae2c657b",
+    "url": "/portfolio/static/css/main.0b9b23b4.chunk.css"
+  },
+  {
+    "revision": "e93e564a3004ad80ad5c",
+    "url": "/portfolio/static/js/2.ba733613.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.ba733613.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29e25cdc603dae2c657b",
+    "url": "/portfolio/static/js/main.92bd0833.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "60fd2330c69fb975a5aaff74b04f2d5f",
+    "url": "/portfolio/static/media/Heckbert-John-Resume-V2.60fd2330.doc"
+  },
+  {
+    "revision": "25a11fe7c02c5bcdf9ed67dfd88cea7c",
+    "url": "/portfolio/static/media/MyPicture.25a11fe7.png"
+  },
+  {
+    "revision": "fdea15de5d1039b6e78d448eb8dadbaa",
+    "url": "/portfolio/static/media/PairProgrammingProject.fdea15de.png"
+  },
+  {
+    "revision": "6e3cc53814e8cf1f3ebb35e82fa3655b",
+    "url": "/portfolio/static/media/Toronto-skyline-sunrise.6e3cc538.jpg"
+  },
+  {
+    "revision": "fd0146d29ca5d42f2b252015ec897f2a",
+    "url": "/portfolio/static/media/capstone-opening.fd0146d2.png"
+  },
+  {
+    "revision": "d1fa89b9151f3962419d7b9a9ef116af",
+    "url": "/portfolio/static/media/capstone-results.d1fa89b9.png"
+  },
+  {
+    "revision": "3dd4da78ed306b5e1788bcad9f4c5a77",
+    "url": "/portfolio/static/media/download-icon.3dd4da78.jpg"
+  }
+]);
