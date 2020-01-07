f66774de41e62fae5af0c1d8023a3baef5b2e514
John Heckbert <johnheckbert@yahoo.ca>
2020-01-07 12:19:50 -0500

    added some comments

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ export default function App() {
     <div className="App">
       <Helmet>
           <meta charSet="utf-8" />
+          <meta> name="description" content="Exciting New Web Developer in York Region"</meta>
+
           <meta name="keywords" content="'web development', 'web developer', 'York Region', GTA, 'mortgage broker', mortgage'" />
           <title>John Heckbert - Exciting New Web Developer in York Region</title>
       </Helmet>
